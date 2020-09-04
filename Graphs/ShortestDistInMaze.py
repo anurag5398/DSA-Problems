c56e42ecc050615ca54a4d77e9d1205832efeb1f
anurag5398 <anurags2898@gmail.com>
2020-09-04 17:25:01 +0530

    Update ShortestDistInMaze.py

--- Graphs/ShortestDistInMaze.py
+++ Graphs/ShortestDistInMaze.py
@@ -7,82 +7,107 @@ Find the shortest distance for the ball to stop at the destination. The distance
 """
 from collections import defaultdict
 from heapq import heappop, heappush
+class Node:
+    def __init__(self, x, y):
+        self.i = x
+        self.j = y
+    def __lt__(self, obj):
+        return self.i <= obj.i
+
 class Solution:
-    def formedge(self, i, j,  A):
-        if i >= self.n or j >= self.m: return []
-        edges = []
-        def nodename(i , j):
-            return 100 * i + j
-        ti, tj = i, j
-        while ti - 1 >= 0 and A[ti - 1][tj] == 0:
-            ti-=1
-        if ti != i: edges.append((i-ti, nodename(ti, tj)))
-        ti, tj = i, j
-        while tj - 1 >= 0 and A[ti][tj - 1] == 0:
-            tj-=1
-        if j != tj: edges.append((j-tj, nodename(ti, tj)))
-        ti, tj = i, j
-        while ti + 1 < self.n and A[ti + 1][tj] == 0:
-            ti+=1
-        if i != ti: edges.append((ti-i, nodename(ti, tj)))
-        ti, tj = i, j
-        while tj + 1 < self.m and A[ti][tj + 1] == 0:
-            tj+=1
-        if tj != j: edges.append((tj-j, nodename(ti, tj)))
-        return edges
+    def isBound(self, node):
+        if node.i < 0 or node.i >= self.n: return False
+        if node.j < 0 or node.j >= self.m: return False
+        if self.A[node.i][node.j] == 1: return False
+        return True
 
+    def nodeadd(self, n1, n2):
+        return Node(n1.i + n2.i, n1.j + n2.j)
 
+    def nodesub(self, n1, n2):
+        return Node(n1.i - n2.i, n1.j - n2.j)
 
+    def checkzero(self, node):
+        if self.A[node.i][node.j] == 0: return True
+        return False
 
+    def isdiff(self, n1, n2):
+        if n1.i == n2.i and n2.j == n1.j: return False
+        return True
 
+    def move(self, node, parent, d, heap):
+        change = [Node(-1, 0), Node(0, -1), Node(1, 0), Node(0, 1)]
+        for cn in change:
+            td = 0
+            tnode = node
+            newnode = self.nodeadd(tnode, cn)
+            while self.isBound(newnode) and self.isdiff(newnode, parent):
+                tnode = newnode
+                td+=1
+                newnode = self.nodeadd(newnode, cn)
+            if td != 0:
+                if tnode not in self.visited:
+                    heappush(heap, (d + td, tnode, self.nodesub(tnode, cn)))
+                    self.visited(tnode)
 
     def solve(self, A, B, C):
-        edges = defaultdict(list)
+        self.A = A
         self.n, self.m = len(A), len(A[0])
-        #print(self.n)
-        s, e = 10 * B[0] + B[1], 10 * C[0] + C[1]
-        h = [(0, s)]
-        visited = set()
+        totalcells = self.n * self.m
+        goal = Node(C[0], C[1])
+        self.visited = set()
+        self.visited.add()
+        h = [(0, Node(B[0], B[1]), Node(-1, -1))]
         while h:
-            d, n = heappop(h)
-            if n == e: return d
-            visited.add(n)
-            for c in self.formedge(n//10, n%10, A):
-                if c[1] not in visited:
-                    heappush(h, (d + c[0], c[1]))
+            d, n, p = heappop(h)
+            #self.visited.add(n)
+            if d > totalcells//2: return -1
+            self.move(n, p, d, h)
+        
+        return -1
+
+
+class Solution2:
+    def isavail(self, i, j):
+        if i < 0 or i >= self.n: return False
+        if j < 0 or j >= self.m: return False
+        if self.A[i][j] == 1: return False
+        return True
+
+    def solve(self, A, B, C):
+        self.A = A
+        self.n, self.m = len(A), len(A[0])
+        change_i = [0, 0, -1, 1]
+        change_j = [-1, 1, 0, 0]
+        visited = set()
+        visited.add((B[0], B[1]))
+        h = [(0, B[0], B[1])]
+        while h:
+
+            d, i, j = heappop(h)
+            if i == C[0] and j == C[1]: return d
+            for ci, cj in zip(change_i, change_j):
+                ti, tj, td = i, j, 0
+                while self.isavail(ti + ci, tj + cj):
+                    ti+=ci
+                    tj+=cj
+                    td+=1
+                if td != 0 and (ti, tj) not in visited:
+                    heappush(h, (d + td, ti, tj))
+                    visited.add((ti, tj))
         return -1
 
 
 
+
+
 A = [
-  [0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
-  [0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-  [1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0],
-  [0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0],
-  [0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1],
-  [0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0],
-  [1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1],
-  [0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-  [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1],
-  [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 1],
+    [0, 0, 0, 1],
+    [0, 0, 0, 1],
+    [0, 0, 0, 0]
 ]
-B = [ 14, 3 ]
-C = [ 10, 25 ]
-t = Solution()
+B = [ 0, 0 ]
+C = [ 3, 3 ]
+t = Solution2()
 print(t.solve(A, B, C))
