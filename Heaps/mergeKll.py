19b45b3b05f55a0df792a8589592dce76db50693
anurag5398 <anurags2898@gmail.com>
2020-09-14 19:59:09 +0530

    Update mergeKll.py

--- Heaps/mergeKll.py
+++ Heaps/mergeKll.py
@@ -1,22 +1,43 @@
+"""
+Given a list containing head pointers of N sorted linked lists. Merge these N given sorted linked lists and return it as one sorted list.
+"""
+#used heap to select smallest value
+
 import heapq
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Node:
+    def __init__(self, ll):
+        self.val = ll.val
+        self.next = ll.next
+    def __lt__(self, other):
+        return self.val <= other.val
 
 class Solution:
+    # @param A : list of linked list
+    # @return the head node in the linked list
     def mergeKLists(self, A):
         heap = []
         for each in A:
-            heapq.heappush(heap, (each.val, each))
-        head = heap[0]
-        temp = None
-        new = None
+            heapq.heappush(heap, Node(each))
+        n = heapq.heappop(heap)
+        new_head = ListNode(n.val)
+        temp = new_head
+        if n.next:
+            heapq.heappush(heap, Node(n.next))
+        
         while heap:
-            new = heapq.heappop(heap)
-            if temp:
-                temp[1].next = new[1]
-            if new[1].next:
-                heapq.heappush(heap, (new[1].next.val , new[1].next))
-            temp = new
-        return head
+            n = heapq.heappop(heap)
+            temp.next = ListNode(n.val)
+            temp = temp.next
+            if n.next:
+                heapq.heappush(heap, Node(n.next))
+        
+        return new_head
 
 
 
-from collections import deque 
+
