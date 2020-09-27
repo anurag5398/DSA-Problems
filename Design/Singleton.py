a8d9d7e36900b5a9fe344b2e31aab64af6e3c864
anurag5398 <anurags2898@gmail.com>
2020-09-27 07:28:29 +0530

    Create Singleton.py

---
+++ Design/Singleton.py
@@ -0,0 +1,60 @@
+
+#SINGLETON
+#JUST because we are assigning __dict__ value, all instance point to same
+#MONOSTATE 
+class Test:
+    __shared = dict()
+    def __init__(self, word):
+        self.__dict__ = self.__shared
+        self.state = word
+    
+    def __number(self, temp):
+        return temp
+    no = 2213
+
+    def check(self):
+        print(self.state)
+
+p1 = Test("abc")
+p2 = Test("xyz")
+
+print(p1.state, p2.state)
+print(p1._Test__shared)
+
+
+#FACTORY
+class CarType:
+    def wheels(self):
+        print("Has 4 wheels")
+
+class BikeType:
+    def wheels(self):
+        print("Has 2 wheels")
+
+class AutoType:
+    def wheels(self):
+        print("HAs 3 wheels")
+
+def factory(vehicle = "Car"):
+    types = {
+        "Car" : CarType,
+        "Bike" : BikeType,
+        "Auto" : AutoType
+    }
+
+    return types[vehicle]()
+
+car = "Car"
+c = factory(car)
+a = factory("Auto")
+b = factory("Bike")
+
+a.wheels()
+b.wheels()
+
+
+fn = "check"
+
+
+
+
